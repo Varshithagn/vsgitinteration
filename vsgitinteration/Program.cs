394e2243d3851c03d9d159c1d579935c0bc5bafa
Varshithagn <varshitha2728@gmail.com>
2021-11-01 12:09:12 +0530

    off

--- vsgitinteration/Program.cs
+++ vsgitinteration/Program.cs
@@ -9,7 +9,7 @@ namespace vsgitinteration
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello!");
             Console.WriteLine("varsha");
-            Console.WriteLine("from gitttt");
+            Console.WriteLine("from github");
             Console.ReadLine();
         }
     }
